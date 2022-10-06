beab0e219297c407a289e55a7755bb800059e8d9
Penuel <penuelsamuel@gmail.com>
2022-10-06 10:29:50 +0100

    added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
 function App() {
+
+  const [query, setQuery] = useState("");
+  const [anime, setAnime] = useState([]);
+
+  
+  const animeDb = async (title) => {
+    const response = await fetch(`https://anime-db.p.rapidapi.com/anime?page=1&size=10&search=+${title}`, {
+      "method": 'GET',
+      "headers": {
+        'X-RapidAPI-Key': 'ef28b3b778msha32a8d3ad262dabp16fe9cjsn2b06ac25740e',
+        'X-RapidAPI-Host': 'anime-db.p.rapidapi.com'
+      }
+    })
+  const data = await response.json()
+  setAnime(data.data)
+    
+  }
+
+  useEffect(() => {
+    animeDb("Fullmetal")
+  },[])
+
+  const searchAnime = (e) => {
+    setQuery(e.target.value)
+  }
+
+  const clickHandler = (e) => {
+    e.preventDefault();
+    animeDb(query)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>ANIME-APP</h1>
+      <form>
+        <input type="text" value={query} placeholder="search for your favourite anime" onChange={searchAnime} />
+        <button className="button" onClick={clickHandler }>Search</button>
+      </form>
+      <div  className='image-container'>
+      {anime.map((ani,index) => (
+          <div key={index} className="image-card">
+          <img src={ani.image} alt={ani.title} />
+          <h2>{ani.title}</h2>
+          </div>
+      ))}
+      </div>
     </div>
   );
 }
